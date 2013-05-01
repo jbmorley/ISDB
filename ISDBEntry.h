1ecc97f20d8bac49d6ac8e6452017e4e36c1304b
Jason Barrie Morley <jason.morley@inseven.co.uk>
2013-05-01 21:46:31 +0100

    Introducing a new ISDBEntry to be returned by the ISDBDataSource implementations. This is intended to make it possible to identify changed entries.

---
+++ ISDBEntry.h
@@ -0,0 +1,20 @@
+//
+//  ISDBEntryIdentifier.h
+//  Popcorn
+//
+//  Created by Jason Barrie Morley on 01/05/2013.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ISDBEntry : NSObject
+
+@property (strong, nonatomic, readonly) id identifier;
+
++ (id)identifierWithIdentifier:(id)identifier
+                       summary:(id)summary;
+- (id)initWithIdentifier:(id)identifier
+                 summary:(id)summary;
+
+@end
---
+++ ISDBEntry.m
@@ -0,0 +1,59 @@
+//
+//  ISDBEntryIdentifier.m
+//  Popcorn
+//
+//  Created by Jason Barrie Morley on 01/05/2013.
+//
+//
+
+#import "ISDBEntry.h"
+
+@interface ISDBEntry ()
+
+@property (strong, nonatomic, readonly) id summary;
+
+@end
+
+
+@implementation ISDBEntry
+
+
++ (id)identifierWithIdentifier:(id)identifier
+                       summary:(id)summary
+{
+  return [[self alloc] initWithIdentifier:identifier
+                                  summary:summary];
+}
+
+
+- (id)initWithIdentifier:(id)identifier
+                 summary:(id)summary
+{
+  self = [super init];
+  if (self) {
+    _identifier = identifier;
+    _summary = summary;
+  }
+  return self;
+}
+
+
+- (BOOL)isEqual:(id)object
+{
+  if (self == object) {
+    return YES;
+  } else if ([self class] == [object class]) {
+    ISDBEntry *identifier = (ISDBEntry *)object;
+    return [self.identifier isEqual:identifier.identifier];
+  }
+  return NO;
+}
+
+
+- (BOOL)isSummaryEqual:(ISDBEntry *)object
+{
+  return [self.summary isEqual:object.summary];
+}
+
+
+@end
